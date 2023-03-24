e65358d9d80555eb08d898aa184a2fc25edd21a6
Alan Cuenca <alan.r.cuenca@gmail.com>
2023-03-24 11:23:51 -0700

    moved category component into folder

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
 import './categories.styles.scss'
+import CategoryItem from './components/category-item/Category-item.component';
+import categories from './data/categories.json'
 
 const App = () => {
 
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Women's",
-    },
-    {
-      id: 5,
-      title: "Men's",
-    }
-  ]
-
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => (
-        <div className="category-container" key={id}>
-          {/* <img /> */}
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Show Now</p>
-          </div>
-        </div>
-
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
-
 
   );
 }
