f9029729de3b1bde8f3212ce69bf59d45cc24ece
Alan Cuenca <alan.r.cuenca@gmail.com>
2023-03-30 16:11:44 -0700

    leverage sign out

--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -2,11 +2,16 @@ import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { UserContext } from "../../Context/UserContext";
 import './navigation.scss';
-import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import { signOutUser } from "../../utilities/firebase/firebase";
 
 const Routes = () => {
-    const { currentUser } = useContext(UserContext);
-    console.log(currentUser);
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+
+    const signOutHandler = async () => {
+        await signOutUser();
+        setCurrentUser(null);
+    };
 
     return (
         <>
@@ -20,9 +25,19 @@ const Routes = () => {
                     <Link className="link" to="/shop">
                         SHOP
                     </Link>
-                    <Link className="link" to="/auth">
-                        Sign-In
-                    </Link>
+                    {
+                        currentUser ? (
+                            <span className="link"
+                                onClick={signOutHandler}
+                            >
+                                Sign-Out
+                            </span>
+                        ) : (
+                            <Link className="link" to="/auth">
+                                Sign-In
+                            </Link>
+                        )
+                    }
                 </div>
             </div>
             <Outlet />
--- src/utilities/firebase/firebase.js
+++ src/utilities/firebase/firebase.js
@@ -5,7 +5,8 @@ import {
     signInWithPopup,
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
-    signInWithEmailAndPassword
+    signInWithEmailAndPassword,
+    signOut
 } from 'firebase/auth';
 
 import {
@@ -76,3 +77,5 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
     return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const signOutUser = async () => await signOut(auth);
